8e05ad464caa5b8c980e09125012228e0a2a3216
borus-dev <smkbcross@gmail.com>
2023-06-19 15:11:07 +0200

    chore: added localhost and automining to fork config

--- hardhat-fork.config.ts
+++ hardhat-fork.config.ts
@@ -9,6 +9,17 @@ export default {
             forking: {
                 url: process.env.NODE_URL || "",
             },
+            // needed for testing deploy scripts locally
+            mining: {
+                auto: true,
+                interval: 1000,
+            },
+        },
+        localhost: {
+            chainId: 1,
+            url: "http://127.0.0.1:8545/",
+            allowUnlimitedContractSize: false,
+            timeout: 1000 * 60,
         },
     },
     mocha: {
