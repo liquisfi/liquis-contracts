cab029d8ea25e5afd92068de391f1850dd3e8d3e
borus-dev <smkbcross@gmail.com>
2023-06-28 18:59:43 +0200

    feat: added script to increase time

---
+++ deploy/deploy/increaseTime.ts
@@ -0,0 +1,38 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+import { getConfig } from "../config";
+
+import { PrelaunchRewardsPool__factory } from "../../types/generated";
+
+const config = getConfig();
+
+// yarn hardhat deploy --network tenderly --no-compile --reset --tags increaseTime
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const deployer = (await ethers.getSigners())[0];
+
+    console.log(`Increasing time in network ${hre.network.name}`);
+
+    const prelaunchRewardsPool = PrelaunchRewardsPool__factory.connect(
+        config.Deployments.prelaunchRewardsPool,
+        deployer,
+    );
+
+    const START_VESTING_DATE = await prelaunchRewardsPool.START_VESTING_DATE();
+    console.log(`Start of vesting date: ${START_VESTING_DATE.toString()}`);
+
+    let block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber());
+    console.log("Current timestamp: ", block.timestamp);
+
+    const duration = parseInt(START_VESTING_DATE.toString()) || 86400;
+
+    await ethers.provider.send("evm_increaseTime", [86400]);
+    await ethers.provider.send("evm_mine", []);
+
+    block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber());
+    console.log("New timestamp:", block.timestamp);
+};
+
+export default func;
+func.tags = ["increaseTime"];
