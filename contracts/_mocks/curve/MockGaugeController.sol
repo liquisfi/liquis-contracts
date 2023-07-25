86c91e61e995562866f71d1868c6c35aeeacfdbc
borus-dev <smkbcross@gmail.com>
2023-07-25 14:19:26 +0200

    fix: solved solidity compiler warnings

--- contracts/_mocks/curve/MockGaugeController.sol
+++ contracts/_mocks/curve/MockGaugeController.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.11;
 
 contract MockGaugeController {
-    function get_gauge_weight(address) external view returns (uint256) {
+    function get_gauge_weight(address) external pure returns (uint256) {
         return 1;
     }
 }
