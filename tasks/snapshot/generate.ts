61ac2de525b413523c2271f26ba042f6aea71305
borus-dev <smkbcross@gmail.com>
2023-08-14 14:25:02 +0200

    feat: work in voting scripts and utils

--- tasks/snapshot/generate.ts
+++ tasks/snapshot/generate.ts
@@ -7,14 +7,22 @@ import {
     getGaugeSnapshot,
     parseLabel,
     saveGaugeChoices,
+    saveGaugeSnapshot,
     sortGaugeList,
     compareAddresses,
     GaugeChoice,
+    Gauge,
 } from "./utils";
 import { getSigner } from "../utils";
 import { config } from "../deploy/mainnet-config";
 import { HardhatRuntime } from "../utils/networkAddressFactory";
-import { IGaugeController__factory, MockCurveGauge__factory, ERC20__factory } from "../../types";
+import {
+    IGaugeController__factory,
+    MockCurveGauge__factory,
+    BunniToken__factory,
+    IUniswapV3PoolImmutables__factory,
+    ERC20__factory,
+} from "../../types";
 import { removedGauges, validNetworks } from "./constants";
 import { uniqBy } from "lodash";
 
@@ -82,7 +90,7 @@ task("snapshot:validate").setAction(async function (_: TaskArguments, hre: Hardh
     }
 });
 
-task("gauges:print").setAction(async function (_: TaskArguments, hre: HardhatRuntime) {
+task("gauges:choices:print").setAction(async function (_: TaskArguments, hre: HardhatRuntime) {
     const signer = await getSigner(hre);
     const gauges = getGaugeChoices();
 
@@ -113,7 +121,7 @@ task("gauges:print").setAction(async function (_: TaskArguments, hre: HardhatRun
 
         try {
             const lpTokenAddress = await gauge.lp_token();
-            const lpToken = ERC20__factory.connect(lpTokenAddress, signer);
+            const lpToken = BunniToken__factory.connect(lpTokenAddress, signer);
             const lpTokenName = await lpToken.name();
 
             cleanedGauges.push({ address: gauge.address, label: lpTokenName });
@@ -124,4 +132,72 @@ task("gauges:print").setAction(async function (_: TaskArguments, hre: HardhatRun
     }
 
     console.log(cleanedGauges);
+});
+
+task("gauges:snapshot:print").setAction(async function (_: TaskArguments, hre: HardhatRuntime) {
+    const signer = await getSigner(hre);
+    const gauges = getGaugeChoices();
+
+    const gaugeController = IGaugeController__factory.connect(config.addresses.gaugeController, signer);
+
+    const count = Number((await gaugeController.n_gauges()).toString());
+
+    const snapshotGauges: Gauge[] = [];
+
+    for (let i = 0; i < count; i++) {
+        const addr = await gaugeController.gauges(i);
+        const gauge = new Contract(
+            addr,
+            [
+                "function is_killed() external view returns (bool)",
+                "function lp_token() external view returns (address)",
+            ],
+            signer,
+        );
+
+        if (await gauge.is_killed()) continue;
+
+        const found = gauges.find((g: GaugeChoice) => compareAddresses(addr, g.address));
+        const isRemoved = removedGauges.find(g => compareAddresses(g, addr));
+        if (!found && !isRemoved) {
+            console.log("Missing:", i, addr);
+        }
+
+        try {
+            const lpTokenAddress = await gauge.lp_token();
+            const lpToken = BunniToken__factory.connect(lpTokenAddress, signer);
+            const lpTokenName = await lpToken.name();
+
+            const uniPoolAddress = await lpToken.pool();
+            const uniPool = IUniswapV3PoolImmutables__factory.connect(uniPoolAddress, signer);
+
+            const token0Address = await uniPool.token0();
+            const token1Address = await uniPool.token1();
+
+            const token0 = ERC20__factory.connect(token0Address, signer);
+            const token1 = ERC20__factory.connect(token1Address, signer);
+
+            const token0Symbol = await token0.symbol();
+            const token1Symbol = await token1.symbol();
+
+            const poolType = {
+                tokens: [
+                    { symbol: token0Symbol, address: token0Address },
+                    { symbol: token1Symbol, address: token1Address },
+                ],
+            };
+
+            snapshotGauges.push({ pool: poolType, network: 1, address: gauge.address, label: lpTokenName });
+        } catch (e) {
+            console.log("Print task error:", gauge.address);
+
+            const poolType = {
+                tokens: [{ symbol: "N/A", address: "N/A" }],
+            };
+
+            snapshotGauges.push({ pool: poolType, network: 1, address: gauge.address, label: "MISSING" });
+        }
+    }
+
+    saveGaugeSnapshot(snapshotGauges);
 });
