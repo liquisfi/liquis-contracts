c3ff027a80365fde32e78c060bd5d6f81776500e
borus-dev <smkbcross@gmail.com>
2023-09-08 11:39:24 +0200

    feat: combine boosterHelper with feesHelper

--- contracts/peripheral/BoosterHelper.sol
+++ contracts/peripheral/BoosterHelper.sol
@@ -5,12 +5,13 @@ import { IERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/utils/SafeERC20.sol";
 import { IBooster } from "../interfaces/IBooster.sol";
 import { IRewardStaking } from "../interfaces/IRewardStaking.sol";
+import { IFeeDistributor } from "../interfaces/bunni/IFeeDistributor.sol";
 
 /**
  * @title   BoosterHelper
  * @author  AuraFinance
- * @notice  Invokes booster.earmarkRewards for multiple pools.
- * @dev     Allows anyone to call `earmarkRewards`  via the booster.
+ * @notice  Invokes booster.earmarkRewards for multiple pools and booster.earmarkFees.
+ * @dev     Allows anyone to call `earmarkRewards` & `earmarkFees` via the booster.
  */
 contract BoosterHelper {
     using SafeERC20 for IERC20;
@@ -18,13 +19,20 @@ contract BoosterHelper {
     IBooster public immutable booster;
     address public immutable crv;
 
+    address public immutable voterProxy = 0x37aeB332D6E57112f1BFE36923a7ee670Ee9278b;
+
+    mapping(address => uint256) public lastTokenTimes;
+    IFeeDistributor public feeDistro;
+
     /**
-     * @param _booster      Booster.sol, e.g. 0xF403C135812408BFbE8713b5A23a04b3D48AAE31
-     * @param _crv          Crv  e.g. 0xba100000625a3754423978a60c9317c58a424e3D
+     * @param _booster      Booster.sol, e.g. 0x631e58246A88c3957763e1469cb52f93BC1dDCF2
+     * @param _crv          oLIT  e.g. 0x627fee87d0D9D2c55098A06ac805Db8F98B158Aa
      */
     constructor(address _booster, address _crv) {
         booster = IBooster(_booster);
         crv = _crv;
+
+        feeDistro = IFeeDistributor(0x951f99350d816c0E160A2C71DEfE828BdfC17f12);
     }
 
     function earmarkRewards(uint256[] memory _pids) external returns (uint256) {
@@ -52,6 +60,38 @@ contract BoosterHelper {
             IBooster.PoolInfo memory poolInfo = booster.poolInfo(_pids[i]);
             IRewardStaking baseRewardPool = IRewardStaking(poolInfo.crvRewards);
             baseRewardPool.processIdleRewards();
+        }
+    }
+
+    /**
+     * @dev Claims fees from fee claimer, and pings the booster to distribute.
+     * @param _tokens Token address to claim fees for.
+     * @param _checkpoints Number of checkpoints required previous to claim fees.
+     */
+    function claimFees(IERC20[] memory _tokens, uint256 _checkpoints) external {
+        uint256 len = _tokens.length;
+        require(len > 0, "!_tokens");
+
+        // Checkpoint user n times before claiming fees
+        for (uint256 i = 0; i < _checkpoints; i++) {
+            feeDistro.checkpointUser(voterProxy);
+        }
+
+        for (uint256 i = 0; i < len; i++) {
+            // Validate if the token should be claimed
+            IERC20 token = _tokens[i];
+            uint256 tokenTime = feeDistro.getTokenTimeCursor(token);
+            require(tokenTime > lastTokenTimes[address(token)], "not time yet");
+
+            IBooster.FeeDistro memory feeDist = booster.feeTokens(address(token));
+            uint256 balanceBefore = token.balanceOf(feeDist.rewards);
+
+            booster.earmarkFees(address(token));
+
+            uint256 balanceAfter = token.balanceOf(feeDist.rewards);
+            require((balanceAfter - balanceBefore) > 0, "nothing claimed");
+
+            lastTokenTimes[address(token)] = tokenTime;
         }
     }
 }
